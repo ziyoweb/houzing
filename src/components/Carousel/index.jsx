5c3dc41ef2e0b0de06d06fe1b8ad2379919806f5
ziyoweb <ziyodulloyevm1@gmail.com>
2023-02-04 23:12:17 +0500

    carousel created

---
+++ src/components/Carousel/index.jsx
@@ -0,0 +1,47 @@
+import React, { useRef } from "react";
+import { Container, Arrow, Img, Blur, Content } from "./style";
+import { Carousel } from "antd";
+import house1 from "../../assets/imgs/house1.png";
+import house2 from "../../assets/imgs/house2.png";
+import house3 from "../../assets/imgs/house4.jpg";
+import { Button } from "../Generic";
+
+const GenCarousel = () => {
+  const onChange = (currentSlide) => {
+    console.log(currentSlide);
+  };
+
+  const slider = useRef();
+
+  const onMove = ({
+    target: {
+      dataset: { name },
+    },
+  }) => {
+    if (name === "right") slider.current.next();
+    if (name === "left") slider.current.prev();
+  };
+
+  return (
+    <Container>
+      <Carousel /*autoplay*/ ref={slider} afterChange={onChange}>
+        <Img height={571} src={house1} />
+        <Img height={571} src={house2} />
+        <Img height={571} src={house3} />
+      </Carousel>
+      <Blur />
+      <Content>
+        <Content.Title>Skyper Pool Partment</Content.Title>
+        <Content.Desc>112 Glenwood Ave Hyde Park, Boston, MA</Content.Desc>
+        <Content.Price>$5,250/mo</Content.Price>
+        <Button width={180} type="dark">
+          Read more
+        </Button>
+      </Content>
+      <Arrow data-name="left" onClick={onMove} left />
+      <Arrow data-name="right" onClick={onMove} />
+    </Container>
+  );
+};
+
+export default GenCarousel;
--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Container } from "./style";
 import Filter from "../Filter";
+import GenCarousel from "../../components/Carousel";
 
 const Home = () => {
   return (
     <Container>
       <Filter />
+      <GenCarousel />
     </Container>
   );
 };
