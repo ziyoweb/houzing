15c2ba33e8890ee6e1c829e1e7f423f7fe59b60b
ziyoweb <ziyodulloyevm1@gmail.com>
2023-02-06 15:34:07 +0500

    some sections added

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -2,14 +2,12 @@ import React from "react";
 import { Container } from "./style";
 import Filter from "../Filter";
 import GenCarousel from "../../components/Carousel";
-import HouseCard from "../HouseCard";
 
 const Home = () => {
   return (
     <Container>
       <Filter />
       <GenCarousel />
-      {/* <HouseCard /> */}
     </Container>
   );
 };
